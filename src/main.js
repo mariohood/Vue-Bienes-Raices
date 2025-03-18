a1bb8b3c0247f13cb99739a0f0078ff47e19b9eb
mariohood <manhattan199@hotmail.com>
2025-03-18 20:06:35 -0300

    266 Instalando Firebase

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Firebase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
+
 import App from './App.vue'
 import router from './router'
 
@@ -17,6 +21,12 @@ const app = createApp(App)
 const vuetify = createVuetify ({
   components,
   directives
+})
+
+// Firebase
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()]
 })
 
 app.use(vuetify)

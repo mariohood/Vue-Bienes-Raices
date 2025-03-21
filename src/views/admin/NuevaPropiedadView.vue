ff1c75c6c20e6e2b9d096684eddf241fb34f226c
mariohood <manhattan199@hotmail.com>
2025-03-21 19:50:45 -0300

    281 Mapeando State com os campos do nosso formulario

--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -2,15 +2,23 @@
   import { useForm, useField } from 'vee-validate'
   import { validationSchema, imageSchema } from '@/validation/propiedadSchema'
 
-const { handleSubmit } = useForm({
-  validationSchema : {
-    ...validationSchema,
-    ...imageSchema
-  }
-})
-
   const items = [1,2,3,4,5]
 
+  const { handleSubmit } = useForm({
+    validationSchema : {
+      ...validationSchema,
+      ...imageSchema
+    }
+  })
+
+  const titulo = useField('titulo')
+  const imagen = useField('imagen')
+  const precio = useField('precio')
+  const habitaciones = useField('habitaciones')
+  const wc = useField('wc')
+  const estacionamiento = useField('estacionamiento')
+  const descripcion = useField('descripcion')
+  
   const submit = handleSubmit((values) => {
     console.log(values)
   })
@@ -36,18 +44,24 @@ const { handleSubmit } = useForm({
       <v-text-field
         class="mb-5"
         label="Titulo Propiedad"
+        v-model="titulo.value.value"
+        :error-messages="titulo.errorMessage.value"
       /> 
 
       <v-file-input 
         accept="image/jpeg"
         label="Fotografia"
         prepend-icon="mdi-camera"
-        class="mb-5"      
+        class="mb-5"
+        v-model="imagen.value.value"
+        :error-messages="imagen.errorMessage.value"        
       />
 
       <v-text-field
         class="mb-5"
         label="Precio"
+        v-model="precio.value.value"
+        :error-messages="precio.errorMessage.value"
       /> 
       <v-row>
         <v-col
@@ -57,7 +71,9 @@ const { handleSubmit } = useForm({
           <v-select
             label="Habitaciones"
             class="mb-5"  
-            :items="items"      
+            :items="items"
+            v-model="habitaciones.value.value"
+            :error-messages="habitaciones.errorMessage.value"      
           />
         </v-col>
 
@@ -68,7 +84,9 @@ const { handleSubmit } = useForm({
           <v-select
             label="WC"
             class="mb-5"  
-            :items="items"      
+            :items="items" 
+            v-model="wc.value.value"
+            :error-messages="wc.errorMessage.value"     
           />
         </v-col>
       
@@ -79,12 +97,19 @@ const { handleSubmit } = useForm({
           <v-select
             label="Lugares de Estacionamiento"
             class="mb-5"  
-            :items="items"      
+            :items="items"
+            v-model="estacionamiento.value.value"
+            :error-messages="estacionamiento.errorMessage.value"    
           />
         </v-col>
       </v-row>
 
-      <v-textarea  class="mb-5" label="Descripción"></v-textarea>
+      <v-textarea  
+        class="mb-5" 
+        label="Descripción"
+        v-model="descripcion.value.value"
+        :error-messages="descripcion.errorMessage.value"
+        ></v-textarea>
       <v-checkbox label="Alberca"/>
 
       <v-btn
